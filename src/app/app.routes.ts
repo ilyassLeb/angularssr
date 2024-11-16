b7679f4d0bda93db71444d62273dc697097c7562
ilyas.labhel <l.labhel@attime-technlogies.com>
2024-11-16 20:24:16 +0100

    error route fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 
-
-export const routes: Routes = [
-    {
-      path: '', // Route parent
-      component: MainComponent,
-      children: [
-        { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirection par défaut
-        { path: 'home', component: HomeComponentComponent }, // Composant enfant
-      ],
-    },
-    { path: '**', redirectTo: '' }, // Redirection pour URL inconnue
-  ];
-  
+export const routes: Routes = [];
