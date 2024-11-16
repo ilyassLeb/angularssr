0ec4284464e6d9c4dba12000f15babf6a770e970
ilyas.labhel <l.labhel@attime-technlogies.com>
2024-11-16 20:10:53 +0100

    first commmit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+      path: '', // Route parent
+      component: MainComponent,
+      children: [
+        { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirection par défaut
+        { path: 'home', component: HomeComponentComponent }, // Composant enfant
+      ],
+    },
+    { path: '**', redirectTo: '' }, // Redirection pour URL inconnue
+  ];
+  
